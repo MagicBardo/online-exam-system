a8446beef2ae34aa89e82743604f1b6646f21f21
MagicBardo <MagicBardo@users.noreply.github.com>
2026-06-14 12:25:43 +0200

    delete all input and output files option as clear.py file

---
+++ backend/clear.py
@@ -0,0 +1,9 @@
+import os
+
+default_paths = ['backend/JSON/', 'exams/']
+
+for default_path in default_paths:
+    for item in os.listdir(default_path):
+        path = f"{default_path}{item}"
+        if input(f"You are about to remove the file at {path}. To confirm, please write the path:\n") == path:
+            os.remove(path)
